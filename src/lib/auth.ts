eda2f72a6f214bc1ca86e173b48d89c2bec00be5
Georg <75206704+Georg97@users.noreply.github.com>
2026-02-21 20:39:46 +0100

    added sveltekit cookie plugin to betterauth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,8 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "../index"; // your drizzle instance
+import { sveltekitCookies } from "better-auth/svelte-kit";
+import { getRequestEvent } from "$app/server";
 
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
@@ -16,4 +18,5 @@ export const auth = betterAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         },
     },
+    plugins: [sveltekitCookies(getRequestEvent)], // make sure this is the last plugin in the array
 });
