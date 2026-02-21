777ef42b8453530e4830b09ca5aff3dacc27f2d9
Georg <75206704+Georg97@users.noreply.github.com>
2026-02-21 20:15:32 +0100

    added google auth and email/password for betterauth and env

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,4 +6,14 @@ export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: "sqlite", // or "mysql", "sqlite"
     }),
+    baseURL: process.env.BETTER_AUTH_URL,
+    emailAndPassword: {
+        enabled: true,
+    },
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        },
+    },
 });
