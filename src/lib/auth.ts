9684a2583df84aab2f9eb7917b0f2e0af455ef1a
Georg <75206704+Georg97@users.noreply.github.com>
2026-02-21 20:55:16 +0100

    fixed drizzle based auth for betterauth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,14 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "../index"; // your drizzle instance
+import * as schema from "../db/schema";
 import { sveltekitCookies } from "better-auth/svelte-kit";
 import { getRequestEvent } from "$app/server";
 
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
-        provider: "sqlite", // or "mysql", "sqlite"
+        provider: "sqlite",
+        schema,
     }),
     baseURL: process.env.BETTER_AUTH_URL,
     emailAndPassword: {
